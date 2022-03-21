82260522efbde94f29553f1e7976d693f7b62b32
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-03-21 16:34:07 +0200

    registering models in django admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from .models import Product, Category, Company, ProductSize, ProductSite, Comment
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Category)
 admin.site.register(Company)
 admin.site.register(ProductSize)
 admin.site.register(ProductSite)
 admin.site.register(Comment)
+
+admin.site.unregister(Group)
+
+admin.site.site_header = 'Product Review Admin'
