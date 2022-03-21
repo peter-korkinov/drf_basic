1648c56419d1518166e1b6313e7e7764a75ea587
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-03-21 16:28:13 +0200

    registering models in django admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
