9dfc13dc0592d04eb656c45d95329c3ce4cc2496
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-03-21 23:42:04 +0200

    creating serializers for Product and Category, view set for them and adding them to the url paths

---
+++ reviews/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['pk', 'name']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ['pk', 'name', 'category']
