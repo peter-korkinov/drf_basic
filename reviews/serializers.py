cd50b899e41416385dcc67b211549319c223a2b5
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-03-22 15:06:46 +0200

    correction in Product Serializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -10,8 +10,9 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True)
-
     class Meta:
         model = Product
-        fields = ['pk', 'name', 'category']
+        fields = ['pk', 'name', 'category', 'created', 'updated']
+        expandable_fields = {
+            'category': (CategorySerializer, {'many': True})
+        }
