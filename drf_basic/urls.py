4bddaf1e900bc7d1fd36fc45e6c47cd5ba872ab4
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-04-18 17:21:03 +0300

    adding url pattern for media

--- drf_basic/urls.py
+++ drf_basic/urls.py
@@ -14,15 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
-from reviews.views import ProductViewSet
+from reviews.views import ProductViewSet, ImageViewSet
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register(r'product', ProductViewSet, basename='Product')
+router.register(r'image', ImageViewSet, basename='Image')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
