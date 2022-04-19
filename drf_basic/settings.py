f2374947528ca0fe386d753eff18277ec4dc93cc
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-04-20 00:58:24 +0300

    configuration REST_FRAMEWORK setting after installing djangorestframework-simplejwt plugin

--- drf_basic/settings.py
+++ drf_basic/settings.py
@@ -133,7 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
 }
 
 
