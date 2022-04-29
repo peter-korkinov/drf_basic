a019620bec8411b5a95132d04dc9b1f7204a903a
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-04-29 19:02:21 +0300

    creating UpdateUserSerializer

--- auth/serializers.py
+++ auth/serializers.py
@@ -68,6 +68,7 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
 
     def validate_old_password(self, value):
         user = self.context['request'].user
+
         if not user.check_password(value):
             raise serializers.ValidationError({"old_password": "Old password isn't correct"})
 
@@ -75,6 +76,44 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         instance.set_password(validated_data['password'])
+        instance.save()
+
+        return instance
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate_email(self, value):
+        user = self.context['request'].user
+
+        if User.objects.exclude(pk=user.pk).filter(email=value).exists():
+            raise serializers.ValidationError({"email": "This email is already in use."})
+
+        return value
+
+    def validate_username(self, value):
+        user = self.context['request'].user
+
+        if User.objects.exclude(pk=user.pk).filter(username=value).exists():
+            raise serializers.ValidationError({"username": "This username is already in use."})
+
+        return value
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.email = validated_data['email']
+        instance.username = validated_data['username']
+
         instance.save()
 
         return instance
