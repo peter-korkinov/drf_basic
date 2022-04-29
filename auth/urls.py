27217a4ad6be75a04659c19ddb9d2d5b725a5566
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-04-29 18:24:43 +0300

    creating ChangePasswordView and adding an endpoint in auth\url.py

--- auth/urls.py
+++ auth/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from auth.views import MyObtainTokenPairView, RegisterView
-
+from auth.views import MyObtainTokenPairView, RegisterView, ChangePasswordView
 
 urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
 ]
