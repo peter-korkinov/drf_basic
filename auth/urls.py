1c68c037b2819afce635a0a3d56d628b2cae38a4
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-05-09 18:31:02 +0300

    Correcting a typo

--- auth/urls.py
+++ auth/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
-    path('update_profile/(<int:pk>', UpdateProfileView.as_view(), name='auth_update_profile'),
+    path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
 ]
